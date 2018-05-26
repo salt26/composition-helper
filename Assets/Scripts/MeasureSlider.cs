8774a4842c5c7630cb856b4a850a7cfb5bd007e7
안단태 <48adt@naver.com>
2018-05-26 18:48:36 +0900

    making start scene

---
+++ Assets/Scripts/MeasureSlider.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class MeasureSlider : MonoBehaviour {
+
+    public Slider slider;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
