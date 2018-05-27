110e4709427461d93356717ec574e8e072a246b7
안단태 <48adt@naver.com>
2018-05-27 14:56:11 +0900

    add button resources

--- Assets/Scripts/MeasureSlider.cs
+++ Assets/Scripts/MeasureSlider.cs
@@ -6,13 +6,22 @@ using UnityEngine.UI;
 public class MeasureSlider : MonoBehaviour {
 
     public Slider slider;
+    public Button decideButton;
+
 	// Use this for initialization
 	void Start () {
 		
 	}
 	
 	// Update is called once per frame
-	void Update () {
-		
+	void FixedUpdate () {
+		if (slider.value < 2 && decideButton.interactable)
+        {
+            decideButton.interactable = false;
+        }
+        else if (slider.value >= 2 && !decideButton.interactable)
+        {
+            decideButton.interactable = true;
+        }
 	}
 }
