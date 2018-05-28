957f83639826ef20b328c810a4f50f0d2548a16f
안단태 <48adt@naver.com>
2018-05-28 18:21:30 +0900

    start scene complete

--- Assets/Scripts/LongPanel.cs
+++ Assets/Scripts/LongPanel.cs
@@ -2,32 +2,50 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
-using UnityEngine.SceneManagement;
 
 public class LongPanel : MonoBehaviour {
 
     public Slider slider;
     public Button decideButton;
+    public GameObject devPanel;
 
 	// Use this for initialization
 	void Start () {
-		
 	}
 	
 	// Update is called once per frame
 	void FixedUpdate () {
-		if (slider.value < 2 && decideButton.interactable)
+		if (slider.value < 2f && decideButton.interactable)
         {
             decideButton.interactable = false;
         }
-        else if (slider.value >= 2 && !decideButton.interactable)
+        else if (slider.value >= 2f && !decideButton.interactable)
         {
             decideButton.interactable = true;
         }
+
+        if (!Input.GetMouseButton(0))
+        {
+            slider.value = Mathf.Round(slider.value);
+        }
 	}
 
-    public void ChangeSceneToScore()
+    public void Decide()
     {
-        SceneManager.LoadScene("Score");
+        if (Manager.manager != null)
+        {
+            // TODO
+            if (Mathf.Round(slider.value) != 2f)
+            {
+                devPanel.SetActive(true);
+            }
+            else
+            {
+                Manager.manager.SetMeasureAndChangeScene(slider.value);
+            }
+        } else
+        {
+            Debug.LogError("Decide Error! Manager is null!");
+        }
     }
 }
---
+++ Assets/Scripts/Manager.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+#if UNITY_EDITOR
+using UnityEditor;
+#endif
+
+public class Manager : MonoBehaviour {
+
+    public static Manager manager;
+    public GameObject melodyPanel;
+    public bool isChordDriven;
+    public int measure;
+
+	// Use this for initialization
+	void Awake () {
+        manager = this;
+        DontDestroyOnLoad(this);
+	}
+
+    void FixedUpdate()
+    {
+        if ((melodyPanel == null || !melodyPanel.name.Equals("MelodyPanel")) && SceneManager.GetActiveScene().name.Equals("Start"))
+        {
+            melodyPanel = GameObject.Find("MelodyPanel");
+        }
+    }
+
+    public void SetDriven(bool isChord)
+    {
+        isChordDriven = isChord;
+        melodyPanel.SetActive(false);
+    } 
+	
+    public void SetMeasureAndChangeScene(float sliderVal)
+    {
+        float i = Mathf.Round(sliderVal);
+        if (i >= 2f)
+        {
+            measure = 8 * (int)i;
+            SceneManager.LoadScene("Score");
+        }
+        else
+        {
+            Debug.LogError("SetMeasure Error!");
+        }
+    }
+
+    public void Quit()
+    {
+#if UNITY_EDITOR
+        EditorApplication.isPlaying = false;
+#else 
+		Application.Quit();
+#endif
+    }
+}
