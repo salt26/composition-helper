8f08f6310a79ddf61ab79cd3e83133d3593937d8
안단태 <48adt@naver.com>
2018-05-29 01:18:24 +0900

    measure and camera scroll

--- Assets/Scripts/Manager.cs
+++ Assets/Scripts/Manager.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 #if UNITY_EDITOR
 using UnityEditor;
 #endif
@@ -9,14 +10,30 @@ using UnityEditor;
 public class Manager : MonoBehaviour {
 
     public static Manager manager;
-    public GameObject melodyPanel;
-    public bool isChordDriven;
-    public int measure;
 
-	// Use this for initialization
-	void Awake () {
+    /*
+     * staffs[0] : MelodyStaff
+     * staffs[1] : AccompanimentStaff
+     * staffs[2] : ChordStaff
+     */
+    List<Staff> staffs = new List<Staff>();
+    GameObject melodyPanel;
+    GameObject mainCamera;
+    Scrollbar scrollbar;
+    bool isScoreScene;
+
+    bool isChordDriven;
+    int measureNum = 0;
+
+    // Use this for initialization
+    void Awake () {
         manager = this;
         DontDestroyOnLoad(this);
+        isScoreScene = false;
+        for (int i = 0; i < 3; i++)
+        {
+            staffs.Add(null);
+        }
 	}
 
     void FixedUpdate()
@@ -25,28 +42,89 @@ public class Manager : MonoBehaviour {
         {
             melodyPanel = GameObject.Find("MelodyPanel");
         }
+
+        if ((staffs[0] == null || !staffs[0].name.Equals("MelodyStaff")) && SceneManager.GetActiveScene().name.Equals("Score"))
+        {
+            staffs[0] = GameObject.Find("MelodyStaff").GetComponent<Staff>();
+        }
+        if ((staffs[1] == null || !staffs[1].name.Equals("AccompanimentStaff")) && SceneManager.GetActiveScene().name.Equals("Score"))
+        {
+            staffs[1] = GameObject.Find("AccompanimentStaff").GetComponent<Staff>();
+        }
+        if ((staffs[2] == null || !staffs[2].name.Equals("ChordStaff")) && SceneManager.GetActiveScene().name.Equals("Score"))
+        {
+            staffs[2] = GameObject.Find("ChordStaff").GetComponent<Staff>();
+        }
+        if (scrollbar == null && SceneManager.GetActiveScene().name.Equals("Score"))
+        {
+            scrollbar = GameObject.Find("Scrollbar").GetComponent<Scrollbar>();
+        }
+        if ((mainCamera == null || !mainCamera.name.Equals("Main Camera")) && SceneManager.GetActiveScene().name.Equals("Score"))
+        {
+            mainCamera = GameObject.Find("Main Camera");
+        }
+
+        if (!isScoreScene && SceneManager.GetActiveScene().name.Equals("Score"))
+        {
+            isScoreScene = true;
+            foreach (Staff s in staffs)
+            {
+                if (s == null)
+                {
+                    Debug.LogError("staffs are null!");
+                    continue;
+                }
+                s.CreateMeasure(measureNum);
+            }
+            if (scrollbar != null)
+            {
+                scrollbar.value = 0f;
+                if (measureNum <= 1) scrollbar.size = 1f;
+                else scrollbar.size = 1f / ((measureNum - 1) * 11f - 5f);
+            }
+        }
+
+        if (scrollbar != null && mainCamera != null)
+        {
+            mainCamera.GetComponent<Transform>().SetPositionAndRotation(
+                new Vector3((scrollbar.value * ((measureNum - 1) * 11f - 5f)), 0f, -10f), Quaternion.identity);
+        }
     }
 
+    /// <summary>
+    /// isChord가 true이면 화음 우선 작곡 모드로, false이면 멜로디 우선 작곡 모드로 설정합니다.
+    /// 그리고 마디 길이를 묻는 창으로 넘어갑니다.
+    /// </summary>
+    /// <param name="isChord"></param>
     public void SetDriven(bool isChord)
     {
         isChordDriven = isChord;
         melodyPanel.SetActive(false);
     } 
 	
+    /// <summary>
+    /// 슬라이더 값에 따라 마디 수를 설정하고 악보 화면으로 씬을 전환합니다.
+    /// </summary>
+    /// <param name="sliderVal"></param>
     public void SetMeasureAndChangeScene(float sliderVal)
     {
         float i = Mathf.Round(sliderVal);
         if (i >= 2f)
         {
-            measure = 8 * (int)i;
+            measureNum = 8 * (int)i;
             SceneManager.LoadScene("Score");
         }
+        // TODO i == 5f일 때 마디 수 자유
         else
         {
             Debug.LogError("SetMeasure Error!");
         }
     }
+    
 
+    /// <summary>
+    /// 프로그램을 종료합니다.
+    /// </summary>
     public void Quit()
     {
 #if UNITY_EDITOR
---
+++ Assets/Scripts/Staff.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Staff : MonoBehaviour {
+
+    public GameObject measure;
+
+    /// <summary>
+    /// 인자 m만큼 마디를 생성해주는 함수입니다.
+    /// </summary>
+    /// <param name="m"></param>
+    public void CreateMeasure(int m)
+    {
+        GameObject g;
+        for (int i = 0; i < m - 1; i++)
+        {
+            g = Instantiate(measure, GetComponent<Transform>());
+            g.GetComponent<Transform>().localPosition = new Vector3(i * 11f, 0f, 0f);
+        }
+        g = Instantiate(measure, GetComponent<Transform>());
+        g.GetComponent<Transform>().localPosition = new Vector3((m - 1) * 11f, 0f, 0f);
+        g.GetComponent<SpriteRenderer>().sprite = Resources.Load("staff5", typeof(Sprite)) as Sprite;
+    }
+}
