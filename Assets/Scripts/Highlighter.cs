3d05baaca821004402198cb0ee82089fc0344a63
cubelover <jeehak0210@gmail.com>
2018-06-18 18:10:10 +0900

    ClickHere Highlighter

--- Assets/Scripts/Highlighter.cs
+++ Assets/Scripts/Highlighter.cs
@@ -69,5 +69,27 @@ public class Highlighter : MonoBehaviour {
                 }
             }
         }
+        else if (GetComponentInChildren<Text>() != null)
+        {
+            while (true)
+            {
+                int frame = 16;
+                for (int i = 0; i < frame; i++)
+                {
+                    GetComponentInChildren<Text>().color = Color.Lerp(new Color(0.709451f, 0.8980392f, 0.502004f, 1f), new Color(0.8980392f, 0.5019608f, 0.6534078f, 1f), i / (float)frame);
+                    yield return new WaitForFixedUpdate();
+                }
+                for (int i = 0; i < frame; i++)
+                {
+                    GetComponentInChildren<Text>().color = Color.Lerp(new Color(0.8980392f, 0.5019608f, 0.6534078f, 1f), new Color(0.5019608f, 0.6641656f, 0.8980392f, 1f), i / (float)frame);
+                    yield return new WaitForFixedUpdate();
+                }
+                for (int i = 0; i < frame; i++)
+                {
+                    GetComponentInChildren<Text>().color = Color.Lerp(new Color(0.5019608f, 0.6641656f, 0.8980392f, 1f), new Color(0.709451f, 0.8980392f, 0.502004f, 1f), i / (float)frame);
+                    yield return new WaitForFixedUpdate();
+                }
+            }
+        }
     }
 }
